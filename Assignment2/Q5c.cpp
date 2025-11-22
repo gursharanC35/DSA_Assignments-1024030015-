134978d810c10f93694aea4e5a503d537f2de52a
gursharanC35 <gsingh13_be24@thapar.edu>
2025-11-22 16:32:25 +0530

    Create Q5c.cpp

---
+++ Assignment2/Q5c.cpp
@@ -0,0 +1,34 @@
+//Lower triangular Matrix. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of matrix : ";
+    cin >> n;
+
+    int size = n*(n+1)/2;  
+    int lower[size];
+
+    cout << "Enter "<<size<<" elements of lower triangular matrix row-wise: ";
+    for (int i = 0; i < size; i++) {
+        cin >> lower[i];
+    }
+
+    int index=0;
+    cout << "Lower triangular matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i >= j) {
+                cout << lower[index] << " ";
+                index++;
+            } else {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
