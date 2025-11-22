78032271c9bd4e803c8c7f5127db5fbd6d55cc30
gursharanC35 <gsingh13_be24@thapar.edu>
2025-11-22 13:03:32 +0530

    Update Q3.cpp

--- Assignment-1/Q3.cpp
+++ Assignment-1/Q3.cpp
@@ -1,1 +1,16 @@
+// Predict the Output of the following program  
+// int main() 
+// { 
+// int i; 
+// int arr[5] = {1}; 
+// for (i = 0; i < 5; i++) 
+// printf("%d",arr[i]); 
+// return 0; 
+// } 
 
+//OUTPUT
+// arr[0] = 1
+// arr[1] = 0
+// arr[2] = 0
+// arr[3] = 0
+// arr[4] = 0
